60cb1468abe4c04c244cf27c37fdb847cd1df417
T00lio <salvacorp@gmail.com>
2023-05-03 23:22:23 -0500

    ContactForm

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,29 +1,14 @@
-import { useState } from "react";
-import { Container, Row, Col, Button } from "reactstrap";
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
-import CampsitesList from "../features/campsites/CampsitesList";
-import { selectRandomCampsite } from "../features/campsites/campsitesSlice";
+import { Container } from 'reactstrap';
+import CampsitesList from '../features/campsites/CampsitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
-    const [selectedCampsite, toggleCampsite] = useState(selectRandomCampsite());
-
-
     return (
         <Container>
-            <Button onClick={() => toggleCampsite(selectRandomCampsite() )}>
-                Select Random Campsite
-            </Button>
-            <Row>
-                <Col sm='5' md='7'>
-                    <CampsitesList />
-                </Col>
-
-                <Col sm='7' md='5'>
-                    <CampsiteDetail campsite={selectedCampsite} />
-                </Col>
-            </Row>
+            <SubHeader current='Directory' />
+            <CampsitesList />
         </Container>
-    )
-}
+    );
+};
 
 export default CampsitesDirectoryPage;
